88285d3804ca71481693a2e3a2db1fac4e0dae7a
aboura <bouchrabichou638@gmail.com>
2023-08-29 23:06:45 +0200

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import MainContent from "./MainContent";
+import SideMenu from "./SideMenu";
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return <div className="app-container">{/* What components go here? */}
+   <MainContent/>
+  <SideMenu/>
+  </div>;
 }
 
 export default App;
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import TopMenu from "./TopMenu.js";
+import GenrePicker from "./GenrePicker.js";
+import AlbumList from "./AlbumList.js";
 function MainContent() {
-  return <main>{/* What components go here? */}</main>;
+  return <main>{/* What components go here? */}
+    <TopMenu/>
+  <GenrePicker/>
+  <AlbumList/>
+  </main>;
 }
 
 export default MainContent;
